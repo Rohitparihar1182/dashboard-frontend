5bea83b8c9d524facbca1d088228d2858072b18a
Rohit Parihar <rohitsinghparihar967@gmail.com>
2024-09-10 15:16:41 +0530

    update

--- src/components/Line/index.jsx
+++ src/components/Line/index.jsx
@@ -1,5 +1,6 @@
 import React, { useMemo, useState } from 'react';
 import { Line } from 'react-chartjs-2';
+// eslint-disable-next-line no-unused-vars
 import Chart from 'chart.js/auto';
 import Filter from '../common/filter';
 
